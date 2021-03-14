66c1303f2fd3588234de778e22bc83743ee57da8
Scarnex <79904643+Scarnex@users.noreply.github.com>
2021-03-14 15:43:45 +0300

    Add files via upload

--- Project1/functions.py
+++ Project1/functions.py
@@ -1,4 +1,11 @@
-import csv, os, getpass
+'''
+The purpose of this module is to contain all the functions possible to make the main.py work.
+If you run this module, it will test out all the functions.
+Authors: Yousef Alshaibani and Abdelrahman Elsayed
+Date: 14/3/2021
+'''
+
+import csv, os, random
 
 #this allows the use of the csv and txt files on other computers. it reads from this folder instead of the directory
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -10,7 +17,7 @@ txt4_file = os.path.join(THIS_FOLDER, 'input4.txt')
 
 balance = 0 #the balance you start with
 def banner():
-    welcome = "Welcome to UBT Banking"
+    welcome = 'Welcome to UBT Banking'
     print('* '*16)
     print(f'*{welcome:^29s}*')
     print('* '*16)
@@ -30,8 +37,9 @@ def login():
             reader = csv.reader(file)
             username = input('Enter Username: ')
             pin = input('Enter PIN: ')
+            next(reader) #skips the "header" line 
 
-            for col in reader: #reads the username in the first column and the pin in the second column of the csv file
+            for col in reader: #reads the username in the first column and the pin in the second column after the header from the csv file
                 if username == col[0] and pin == col[1]:
                     print('Login successful!')
                     login = True
@@ -40,17 +48,21 @@ def login():
                     print('Try again')
                     break
 
-#writes a row in the csv file for future login
+#writes a row after the header in the csv file for future login
 def create_account():
-    with open(account_file, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow([input('Enter your new Username: '), input('Enter your new PIN: ')])
+    print ('Here is an example of what your PIN should look like:', random.randint(111,999)) #prints random 3 digits as an example of a pin
+    with open(account_file, 'w', newline = '') as file:
+        header = ['Username', 'PIN']
+        writer = csv.DictWriter(file, fieldnames = header)
+        writer.writeheader()
+        writer.writerow({'Username': input('Enter your new Username: '), 'PIN': int(input("Enter your new PIN: "))})     
         print('Account succesfully created!')
 
 #what the user should see after a successful login screen
 def options():
     with open(account_file, 'r') as file: #similar to the login function, it reads the first column of the csv file which is the username
         reader = csv.reader(file)
+        next(reader)
         for col in reader:
             print(f'\nHello, {col[0].title()}. What would you like to do today?') #capitalizes the username for better aesthetics
             break
@@ -86,13 +98,15 @@ def deposit():
     with open(deposit_file, 'a', newline = '') as file: #writes the amount you deposited to the txt file
         writer = csv.writer(file)
         writer.writerow([amount])
-                 
+
+#prints current balance and writes it in the txt file               
 def current_balance():
     with open(balance_file, 'w') as file:
         writer = csv.writer(file)
         writer.writerow([balance])
         print(f'Your balance is {balance:.2f} SR.')
 
+#prints all the amounts you withdrew and deposited throughout using the program
 def transaction_history():
     with open(withdraw_file, 'r') as file: #reads and prints the withdraw.txt file
         print('-'*19)
@@ -105,3 +119,16 @@ def transaction_history():
         print('Deposit History:')
         print('-'*16)
         print(file.read())
+
+if __name__ == '__main__':
+    print('Testing functions...\n')
+    banner()
+    login_screen()
+    create_account()
+    login()
+    options()
+    deposit()
+    withdraw()
+    current_balance()
+    transaction_history()
+    print('Test complete.')
--- Project1/main.py
+++ Project1/main.py
@@ -1,1 +1,50 @@
+'''
+This is a banking app that lets you perform banking services similar to the conventional banking apps
+If you run this module, you'll be able to use our banking services
+Authors: Yousef Alshaibani and Abdelrahman Elsayed
+Date: 14/3/2021
+'''
 
+from functions import banner, login_screen, login, create_account, options, withdraw, deposit, current_balance, transaction_history
+import sys
+
+while True:
+    banner()
+    login_screen()
+    login_choice = int(input('Option: '))
+
+    if login_choice == 1:
+        login()
+        break
+    elif login_choice == 2:
+        create_account()
+        break
+    elif login_choice == 3:
+        sys.exit('Thank you for using UBT banking. Have a good day!')
+    else:
+        print('Invalid option')
+        continue
+        
+while True:
+    options()
+    choice = int(input('Option: '))
+
+    if choice == 1:
+        withdraw()
+    elif choice == 2:
+        deposit()
+    elif choice == 3:
+        current_balance()
+    elif choice == 4:
+        transaction_history()
+    elif choice == 5:
+        sys.exit('Thank you for using UBT banking. Have a good day!')
+    else:
+         print('Invalid option.')
+         continue
+
+    answer = input('\nWould you like to do another transaction? (y/n): ')
+    if answer.lower() == 'y':
+        continue
+    else:
+        sys.exit('Thank you for using UBT banking. Have a good day!')
